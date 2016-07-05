740b3f41e3234d2e8ea391ecdb18b8ce2344139d
LShlikht <LShlikht@luxoft.com>
2016-07-05 09:21:20 +0300

    Cnange .js file

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var sizeOfTable = 3;
 for(var i = 0; i < sizeOfTable; i++)
 {
 	var trOfTable = document.createElement('tr');
-	trOfTable.innerHTML = i;
+	//trOfTable.innerHTML = i;
     document.getElementById("theTable").appendChild(trOfTable);
 	for(var j = 0; j < 4; j++)
 	{
